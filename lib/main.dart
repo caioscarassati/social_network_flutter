d8992d2a30ad238999349ed0f75ea20f0253f790
scarassatipermeets <caio.scarassati@permeets.com>
2025-08-29 18:23:59 -0300

    add flutter_native_splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
+import 'package:uuid/uuid.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:intl/date_symbol_data_local.dart';  
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:social_network/app/data/models/post_cache_model.dart';
 import 'package:social_network/app/data/models/user_cache_model.dart';
 import 'package:social_network/app/data/models/user_model.dart';
@@ -10,11 +11,12 @@ import 'package:social_network/app/data/provider/local_user_provider.dart';
 import 'package:social_network/app/routes/app_pages.dart';
 import 'package:social_network/app/ui/theme/app_theme.dart';
 import 'package:social_network/core/lang/app_translations.dart';
-import 'package:uuid/uuid.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   //  Inicializa a formatação de data ---
   await initializeDateFormatting();
 
@@ -29,6 +31,7 @@ void main() async {
   await _seedInitialUser();
 
   runApp(const MyApp());
+  FlutterNativeSplash.remove();
 }
 
 Future<void> _seedInitialUser() async {
